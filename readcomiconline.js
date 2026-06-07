108612375024098e9007252a8de31a0183097ea8
TrexxyMon <01jlmartin@gmail.com>
2026-06-06 21:27:43 -0400

    Bump ReadComicOnline restored extension version

--- readcomiconline.js
+++ readcomiconline.js
@@ -10,7 +10,7 @@
 __cinderExport = {
 	id: "readcomiconline",
 	name: "ReadComicOnline",
-	version: "1.0.9",
+	version: "1.0.11",
 	icon: "📚",
 	description: "Read Marvel, DC, Image and more comics from ReadComicOnline",
 	contentType: "comics",
